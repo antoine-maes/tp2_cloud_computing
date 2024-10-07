8f70c96594705127c53d718af7d33e4282ad57b2
Antoine Maes <92756820+AntoLeRigoloo@users.noreply.github.com>
2024-10-07 08:43:11 +0200

    push

---
+++ main.tf
@@ -0,0 +1,156 @@
+resource "azurerm_resource_group" "example" {
+  name     = "${random_pet.prefix.id}-rg"
+  location = var.location
+}
+
+resource "azurerm_cosmosdb_account" "example" {
+  name                      = random_pet.prefix.id
+  location                  = var.cosmosdb_account_location
+  resource_group_name       = azurerm_resource_group.example.name
+  offer_type                = "Standard"
+  kind                      = "GlobalDocumentDB"
+  enable_automatic_failover = false
+  enable_free_tier          = true
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+  }
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+  depends_on = [
+    azurerm_resource_group.example
+  ]
+}
+
+resource "azurerm_cosmosdb_sql_database" "main" {
+  name                = "${random_pet.prefix.id}-cosmosdb-sqldb"
+  resource_group_name = azurerm_resource_group.example.name
+  account_name        = azurerm_cosmosdb_account.example.name
+  throughput          = var.throughput
+}
+
+resource "azurerm_cosmosdb_sql_container" "example" {
+  name                  = "${random_pet.prefix.id}-sql-container"
+  resource_group_name   = azurerm_resource_group.example.name
+  account_name          = azurerm_cosmosdb_account.example.name
+  database_name         = azurerm_cosmosdb_sql_database.main.name
+  partition_key_path    = "/definition/id"
+  partition_key_version = 1
+  throughput            = var.throughput
+
+  indexing_policy {
+    indexing_mode = "consistent"
+
+    included_path {
+      path = "/*"
+    }
+
+    included_path {
+      path = "/included/?"
+    }
+
+    excluded_path {
+      path = "/excluded/?"
+    }
+  }
+
+  unique_key {
+    paths = ["/definition/idlong", "/definition/idshort"]
+  }
+}
+
+resource "random_pet" "prefix" {
+  prefix = var.prefix
+  length = 1
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "maesblobstorageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "text_name.txt"
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = "Block"
+  source                 = "text.txt"
+}
+
+
+# APP SERVICE
+
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "webapp-asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "webapp-${random_integer.ri.result}"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
+  }
+}
+
+#  Deploy code from a public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.webapp.id
+  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+  branch             = "master"
+  use_manual_integration = true
+  use_mercurial      = false
+}
+
+
+# resource "azurerm_app_service_plan" "example" {
+#   name                = "maesmyAppServicePlan"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   sku {
+#     tier = "Free"
+#     size = "F1"
+#   }
+# }
+
+# resource "azurerm_linux_web_app" "example" {
+#   name                = "maesmyFlaskApp"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   service_plan_id     = azurerm_app_service_plan.example.id
+#   site_config {
+#     always_on = false
+#   }
+
+#   app_settings = {
+#     "WEBSITE_RUN_FROM_PACKAGE" = "https://${azurerm_storage_account.example.name}.blob.core.windows.net/${azurerm_storage_container.example.name}/${azurerm_storage_blob.example.name}"
+#   }
+# }
+
+
